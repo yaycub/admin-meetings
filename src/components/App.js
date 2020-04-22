fcd0805f08ef1ce3c76a43e8a9e15c74a2287c69
Jacob Harrington <jharrington4@gmail.com>
2020-04-21 18:00:18 -0700

    fixed test

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AdminMeeting from './container/AdminMeetings';
+import styles from './form/Form.css';
 
 export default function App() {
   return (
     <>
-      <h1>Alchemeetings Admin</h1>
+      <h1 className={styles.header}>Alchemeetings Admin</h1>
       <AdminMeeting />
     </>
   );
